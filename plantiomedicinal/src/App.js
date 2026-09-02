812fc8d0ac71d7876d7a3150d8f0f4d21cef4f7f
MATHEUS  HENRIQUE AZEVEDO JUNTINI <0220832611018@fatectq.local>
2026-09-02 12:17:03 -0300

    Iniciação no React (Matheus)

--- plantiomedicinal/src/App.js
+++ plantiomedicinal/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+
+      <main>
+
+      <span className="spanInicio">
+      <img src="./img/logo1.png" />
+      <b>+ de 20 espécies diferentes</b>
+      </span>
+
+      <div className="info">
+
+        <div className="infoTexto">
+
+          <h1>
+            Faça seu jardim <br />
+            <strong>MEDICINAL</strong> <br />
+            agora, na sua casa!
+          </h1>
+
+          <p>
+            Veja as plantas medicinais com segurança. Consulte fichas
+            técnicas detalhadas de espécies vegetais, filtre por
+            características e encontre a planta ideal para cada projeto
+            de plantio.
+          </p>
+
+          <a href="">
+            <button className="infoBotao">
+              <b>SAIBA MAIS!</b>
+            </button>
+          </a>
+
+          </div>
+
+          <div className="infoImagem">
+            <img src="./img/4.png" alt="Logo" />
+          </div>
+
+        </div>
+
+        <hr className="divisao1" />
+        <hr className="divisao2" />
+
+      </main>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ plantiomedicinal/src/components/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+  return (
+    <header className="header">
+
+      <nav>
+        <a href="">Sobre Nós</a>
+        <a href="">Receitas</a>
+        <a href="">Catálogo</a>
+        <a href="">Postagens</a>
+      </nav>
+
+      <div className="logo">
+        <img src="./img/logo1.png" alt="Logo" />
+      </div>
+
+    </header>
+  );
+}
+
+export default Header;
